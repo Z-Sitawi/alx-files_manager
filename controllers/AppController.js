aba042f3f086b2060ee1ea87b55761f1430a27be
Z-sitawi <aaichaou.zakaria@gmail.com>
2024-08-06 15:57:27 +0100

    creating the Express server with the First API

--- controllers/AppController.js
+++ controllers/AppController.js
@@ -1,1 +1,19 @@
 #!/usr/bin/node
+import redisClient from '../utils/redis';
+import dbClient from '../utils/db';
+
+class AppController {
+  static getStatus(req, res) {
+    const redisStatus = redisClient.isAlive();
+    const dbStatus = dbClient.isAlive();
+    res.status(200).json({ redis: redisStatus, db: dbStatus });
+  }
+
+  static async getStats(req, res) {
+    const nbrUsers = await dbClient.nbUsers();
+    const nbrFiles = await dbClient.nbFiles();
+    res.status(200).json({ users: nbrUsers, files: nbrFiles });
+  }
+}
+
+export default AppController;
--- server.js
+++ server.js
@@ -1,1 +1,17 @@
 #!/usr/bin/node
+const express = require('express');
+const routes = require('./routes/index');
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+// Middleware to parse JSON bodies
+app.use(express.json());
+
+// !Use the routes defined in routes/index.js
+app.use('/', routes);
+
+// Start the server
+app.listen(port, 'localhost', () => {
+  console.log(`Server is running at http://localhost:${port}`);
+});
