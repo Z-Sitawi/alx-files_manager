0dd8c255fbfd355f30d9e3e47dfea6d769e8bc11
Z-sitawi <aaichaou.zakaria@gmail.com>
2024-08-06 16:26:51 +0100

    fix

--- server.js
+++ server.js
@@ -3,15 +3,11 @@ import express from 'express';
 import routes from './routes/index';
 
 const app = express();
+app.use(express.json());
 const port = process.env.PORT || 5000;
 
-// Middleware to parse JSON bodies
-app.use(express.json());
-
-// !Use the routes defined in routes/index.js
 app.use('/', routes);
 
-// Start the server
-app.listen(port, 'localhost', () => {
+app.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
