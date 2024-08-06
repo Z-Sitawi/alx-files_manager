3284ce690064200a84e7a7a593b6ec033bee6212
Z-sitawi <aaichaou.zakaria@gmail.com>
2024-08-07 00:30:34 +0100

    fix

---
+++ controllers/AppController.js
@@ -0,0 +1,18 @@
+import dbClient from '../utils/db';
+import redisClient from '../utils/redis';
+
+class AppController {
+  static getStatus(req, res) {
+    const redisStatus = redisClient.isAlive();
+    const dbStatus = dbClient.isAlive();
+    res.status(200).json({ redis: redisStatus, db: dbStatus });
+  }
+
+  static async getStats(req, res) {
+    const nbrUsers = await dbClient.nbUsers();
+    const nbrFiles = await dbClient.nbFiles();
+    res.status(200).json({ users: nbrUsers, files: nbrFiles });
+  }
+}
+
+export default AppController;
